dcab6c55ec3e71a0831ebbae5e3390f955ae3acd
Ole Morten Haaland <ole.morten.haaland@nordicsemi.no>
2013-11-27 10:50:53 +0100

    Added project as-is from Developer Zone.
    From post at https://devzone.nordicsemi.com/index.php/broadcaster-mode

---
+++ main.c
@@ -0,0 +1,356 @@
+/* Copyright (c) 2012 Nordic Semiconductor. All Rights Reserved.
+ *
+ * The information contained herein is property of Nordic Semiconductor ASA.
+ * Terms and conditions of usage are described in detail in NORDIC
+ * SEMICONDUCTOR STANDARD SOFTWARE LICENSE AGREEMENT.
+ *
+ * Licensees are granted free, non-transferable use of the information. NO
+ * WARRANTY of ANY KIND is provided. This heading must NOT be removed from
+ * the file.
+ *
+ */
+
+/** @file
+ *
+ * @defgroup ble_sdk_app_template_main main.c
+ * @{
+ * @ingroup ble_sdk_app_template
+ * @brief Template project main file.
+ *
+ * This file contains a template for creating a new application. It has the code necessary to wakeup
+ * from button, advertise, get a connection restart advertising on disconnect and if no new 
+ * connection created go back to system-off mode.
+ * It can easily be used as a starting point for creating a new application, the comments identified
+ * with 'YOUR_JOB' indicates where and how you can customize.
+ */
+
+#include <stdint.h>
+#include <string.h>
+#include "nordic_common.h"
+#include "nrf.h"
+#include "app_util.h"
+#include "app_error.h"
+#include "nrf_gpio.h"
+#include "nrf51_bitfields.h"
+#include "ble.h"
+#include "ble_hci.h"
+#include "ble_srv_common.h"
+#include "ble_advdata.h"
+#include "ble_nrf6310_pins.h"
+#include "ble_stack_handler.h"
+#include "app_timer.h"
+#include "ble_error_log.h"
+#include "ble_debug_assert_handler.h"
+#include "nrf_temp.h"
+
+#define ADV_INTERVAL_IN_MS              1200
+#define VBAT_MAX_IN_MV                  3300
+
+#define ADV_INTERVAL                    MSEC_TO_UNITS(ADV_INTERVAL_IN_MS, UNIT_0_625_MS) /**< The advertising interval (in units of 0.625 ms. */
+#define ADV_TIMEOUT_IN_SECONDS          0                                           /**< The advertising timeout (in units of seconds). */
+
+#define APP_TIMER_PRESCALER             0                                           /**< Value of the RTC1 PRESCALER register. */
+#define APP_TIMER_MAX_TIMERS            1                                           /**< Maximum number of simultaneously created timers. */
+#define APP_TIMER_OP_QUEUE_SIZE         4                                           /**< Size of timer operation queues. */
+#define ADVDATA_UPDATE_INTERVAL         APP_TIMER_TICKS(ADV_INTERVAL_IN_MS, APP_TIMER_PRESCALER)
+
+#define DEAD_BEEF                       0xDEADBEEF                                  /**< Value used as error code on stack dump, can be used to identify stack location on stack unwind. */
+
+
+static app_timer_id_t m_advdata_update_timer;
+
+/**@brief Function for error handling, which is called when an error has occurred. 
+ *
+ * @warning This handler is an example only and does not fit a final product. You need to analyze 
+ *          how your product is supposed to react in case of error.
+ *
+ * @param[in] error_code  Error code supplied to the handler.
+ * @param[in] line_num    Line number where the handler is called.
+ * @param[in] p_file_name Pointer to the file name. 
+ */
+void app_error_handler(uint32_t error_code, uint32_t line_num, const uint8_t * p_file_name)
+{
+    nrf_gpio_pin_set(ASSERT_LED_PIN_NO);
+
+    // This call can be used for debug purposes during development of an application.
+    // @note CAUTION: Activating this code will write the stack to flash on an error.
+    //                This function should NOT be used in a final product.
+    //                It is intended STRICTLY for development/debugging purposes.
+    //                The flash write will happen EVEN if the radio is active, thus interrupting
+    //                any communication.
+    //                Use with care. Un-comment the line below to use.
+    ble_debug_assert_handler(error_code, line_num, p_file_name);
+
+    // On assert, the system can only recover with a reset.
+    //NVIC_SystemReset();
+}
+
+
+/**@brief Callback function for asserts in the SoftDevice.
+ *
+ * @details This function will be called in case of an assert in the SoftDevice.
+ *
+ * @warning This handler is an example only and does not fit a final product. You need to analyze 
+ *          how your product is supposed to react in case of Assert.
+ * @warning On assert from the SoftDevice, the system can only recover on reset.
+ *
+ * @param[in]   line_num   Line number of the failing ASSERT call.
+ * @param[in]   file_name  File name of the failing ASSERT call.
+ */
+void assert_nrf_callback(uint16_t line_num, const uint8_t * p_file_name)
+{
+    app_error_handler(DEAD_BEEF, line_num, p_file_name);
+}
+
+/**@brief Function for the LEDs initialization.
+ *
+ * @details Initializes all LEDs used by the application.
+ */
+static void leds_init(void)
+{
+    GPIO_LED_CONFIG(ADVERTISING_LED_PIN_NO);
+    GPIO_LED_CONFIG(CONNECTED_LED_PIN_NO);
+    GPIO_LED_CONFIG(ASSERT_LED_PIN_NO);
+}
+
+/**@brief Function for the GAP initialization.
+ *
+ * @details This function shall be used to setup all the necessary GAP (Generic Access Profile) 
+ *          parameters of the device. It also sets the permissions and appearance.
+ */
+static void gap_params_init(void)
+{
+    uint32_t                err_code;
+    ble_gap_conn_sec_mode_t sec_mode;
+    
+    char name_buffer[9];
+    sprintf(name_buffer, "%08X", NRF_FICR->DEVICEID[0]);
+    
+    BLE_GAP_CONN_SEC_MODE_SET_OPEN(&sec_mode);
+    
+    err_code = sd_ble_gap_device_name_set(&sec_mode,
+                                          (const uint8_t *)name_buffer, 
+                                          strlen(name_buffer));
+    APP_ERROR_CHECK(err_code);  
+}
+
+uint8_t battery_level_get(void)
+{
+    // Configure ADC
+    NRF_ADC->INTENSET   = ADC_INTENSET_END_Msk;
+    NRF_ADC->CONFIG     = (ADC_CONFIG_RES_8bit                        << ADC_CONFIG_RES_Pos)     |
+                          (ADC_CONFIG_INPSEL_SupplyOneThirdPrescaling << ADC_CONFIG_INPSEL_Pos)  |
+                          (ADC_CONFIG_REFSEL_VBG                      << ADC_CONFIG_REFSEL_Pos)  |
+                          (ADC_CONFIG_PSEL_Disabled                   << ADC_CONFIG_PSEL_Pos)    |
+                          (ADC_CONFIG_EXTREFSEL_None                  << ADC_CONFIG_EXTREFSEL_Pos);
+    NRF_ADC->EVENTS_END = 0;
+    NRF_ADC->ENABLE     = ADC_ENABLE_ENABLE_Enabled;
+
+    NRF_ADC->EVENTS_END  = 0;    // Stop any running conversions.
+    NRF_ADC->TASKS_START = 1;
+    
+    while (!NRF_ADC->EVENTS_END)
+    {
+    }
+    
+    uint16_t vbg_in_mv = 1200;
+    uint8_t adc_max = 255;
+    uint16_t vbat_current_in_mv = (NRF_ADC->RESULT * 3 * vbg_in_mv) / adc_max;
+    
+    NRF_ADC->EVENTS_END     = 0;
+    NRF_ADC->TASKS_STOP     = 1;
+    
+    return (uint8_t) ((vbat_current_in_mv * 100) / VBAT_MAX_IN_MV);
+}
+
+uint32_t temperature_data_get(void)
+{
+    uint32_t temp;
+    NRF_TEMP->TASKS_START = 1; /** Start the temperature measurement. */
+
+    /* Busy wait while temperature measurement is not finished, you can skip waiting if you enable interrupt for DATARDY event and read the result in the interrupt. */
+    /*lint -e{845} // A zero has been given as right argument to operator '|'" */
+    while (NRF_TEMP->EVENTS_DATARDY == 0)            
+    {
+        // Do nothing.
+    }
+    NRF_TEMP->EVENTS_DATARDY = 0;  
+    
+    /**@note Workaround for PAN_028 rev2.0A anomaly 29 - TEMP: Stop task clears the TEMP register. */       
+    temp = (nrf_temp_read() / 4) * 100;
+    
+    /**@note Workaround for PAN_028 rev2.0A anomaly 30 - TEMP: Temp module analog front end does not power down when DATARDY event occurs. */
+    NRF_TEMP->TASKS_STOP = 1; /** Stop the temperature measurement. */
+    
+    int8_t exponent = -2;
+    return ((exponent & 0xFF) << 24) | temp & 0x00FFFFFF;
+}
+
+
+/**@brief Function for initializing the Advertising functionality.
+ *
+ * @details Encodes the required advertising data and passes it to the stack.
+ *          Also builds a structure to be passed to the stack when starting advertising.
+ */
+static void advdata_update(void)
+{
+    uint32_t      err_code;
+    ble_advdata_t advdata;
+    uint8_t       flags = BLE_GAP_ADV_FLAG_BR_EDR_NOT_SUPPORTED;
+    
+    ble_advdata_service_data_t service_data[2];
+    
+    uint8_t battery_data = battery_level_get();
+    uint32_t temperature_data = temperature_data_get();
+    
+    service_data[0].service_uuid = BLE_UUID_BATTERY_SERVICE;
+    service_data[0].data.size    = 1;
+    service_data[0].data.p_data  = &battery_data;
+
+    service_data[1].service_uuid = BLE_UUID_HEALTH_THERMOMETER_SERVICE;
+    service_data[1].data.size    = 4;
+    service_data[1].data.p_data  = (uint8_t *) &temperature_data;
+
+    // Build and set advertising data
+    memset(&advdata, 0, sizeof(advdata));
+
+    advdata.name_type            = BLE_ADVDATA_FULL_NAME;
+    advdata.include_appearance   = false;
+    advdata.flags.size           = sizeof(flags);
+    advdata.flags.p_data         = &flags;
+    advdata.service_data_count   = 2;
+    advdata.p_service_data_array = service_data;
+
+    err_code = ble_advdata_set(&advdata, NULL);
+    APP_ERROR_CHECK(err_code);
+}
+
+void advdata_update_timer_timeout_handler(void * p_context)
+{
+    advdata_update();
+}
+
+/**@brief Function for the Timer initialization.
+ *
+ * @details Initializes the timer module.
+ */
+static void timers_init(void)
+{
+    // Initialize timer module, making it use the scheduler
+    APP_TIMER_INIT(APP_TIMER_PRESCALER, APP_TIMER_MAX_TIMERS, APP_TIMER_OP_QUEUE_SIZE, false);
+    
+    uint32_t err_code = app_timer_create(&m_advdata_update_timer, APP_TIMER_MODE_REPEATED, advdata_update_timer_timeout_handler);
+    APP_ERROR_CHECK(err_code);
+}
+
+
+/**@brief Function for starting timers.
+*/
+static void timers_start(void)
+{
+    uint32_t err_code = app_timer_start(m_advdata_update_timer, ADVDATA_UPDATE_INTERVAL, NULL);
+    APP_ERROR_CHECK(err_code);
+}
+
+
+/**@brief Function for starting advertising.
+ */
+static void advertising_start(void)
+{
+    uint32_t             err_code;
+    ble_gap_adv_params_t adv_params;
+    
+    // Start advertising
+    memset(&adv_params, 0, sizeof(adv_params));
+    
+    adv_params.type        = BLE_GAP_ADV_TYPE_ADV_NONCONN_IND;
+    adv_params.p_peer_addr = NULL;
+    adv_params.fp          = BLE_GAP_ADV_FP_ANY;
+    adv_params.interval    = ADV_INTERVAL;
+    adv_params.timeout     = ADV_TIMEOUT_IN_SECONDS;
+
+    err_code = sd_ble_gap_adv_start(&adv_params);
+    APP_ERROR_CHECK(err_code);
+    nrf_gpio_pin_set(ADVERTISING_LED_PIN_NO);
+}
+
+
+/**@brief Function for handling the Application's BLE Stack events.
+ *
+ * @param[in]   p_ble_evt   Bluetooth stack event.
+ */
+static void on_ble_evt(ble_evt_t * p_ble_evt)
+{
+    uint32_t                         err_code = NRF_SUCCESS;
+    
+    switch (p_ble_evt->header.evt_id)
+    {
+        default:
+            break;
+    }
+
+    APP_ERROR_CHECK(err_code);
+}
+
+
+/**@brief Function for dispatching a BLE stack event to all modules with a BLE stack event handler.
+ *
+ * @details This function is called from the scheduler in the main loop after a BLE stack
+ *          event has been received.
+ *
+ * @param[in]   p_ble_evt   Bluetooth stack event.
+ */
+static void ble_evt_dispatch(ble_evt_t * p_ble_evt)
+{
+    on_ble_evt(p_ble_evt);
+}
+
+
+/**@brief Function for initializing the BLE stack.
+ *
+ * @details Initializes the SoftDevice and the BLE event interrupt.
+ */
+static void ble_stack_init(void)
+{
+    BLE_STACK_HANDLER_INIT(NRF_CLOCK_LFCLKSRC_XTAL_20_PPM,
+                           BLE_L2CAP_MTU_DEF,
+                           ble_evt_dispatch,
+                           false);
+}
+
+
+/**@brief Function for the Power manager.
+ */
+static void power_manage(void)
+{
+    uint32_t err_code = sd_app_event_wait();
+    APP_ERROR_CHECK(err_code);
+}
+
+
+/**@brief Function for application main entry.
+ */
+int main(void)
+{
+    // Initialize
+    nrf_temp_init();
+    leds_init();
+    ble_stack_init();
+    gap_params_init();
+    timers_init();
+    advdata_update();
+    
+    // Start execution
+    timers_start();
+    advertising_start();
+    
+    // Enter main loop
+    for (;;)
+    {
+        power_manage();
+    }
+}
+
+/** 
+ * @}
+ */
