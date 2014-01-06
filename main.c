86b407e1f0b96aeb8a8286d2431edcf5b1689556
Ole Morten Haaland <ole.morten.haaland@nordicsemi.no>
2014-01-06 17:15:23 +0100

    Updated license header of main.c.

--- main.c
+++ main.c
@@ -1,27 +1,11 @@
 /* Copyright (c) 2012 Nordic Semiconductor. All Rights Reserved.
  *
- * The information contained herein is property of Nordic Semiconductor ASA.
- * Terms and conditions of usage are described in detail in NORDIC
- * SEMICONDUCTOR STANDARD SOFTWARE LICENSE AGREEMENT.
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms given in LICENSE.md in the root folder of the 
+ * project.
  *
- * Licensees are granted free, non-transferable use of the information. NO
- * WARRANTY of ANY KIND is provided. This heading must NOT be removed from
- * the file.
+ * See README.md for a description of the application. 
  *
- */
-
-/** @file
- *
- * @defgroup ble_sdk_app_template_main main.c
- * @{
- * @ingroup ble_sdk_app_template
- * @brief Template project main file.
- *
- * This file contains a template for creating a new application. It has the code necessary to wakeup
- * from button, advertise, get a connection restart advertising on disconnect and if no new 
- * connection created go back to system-off mode.
- * It can easily be used as a starting point for creating a new application, the comments identified
- * with 'YOUR_JOB' indicates where and how you can customize.
  */
 
 #include <stdint.h>
